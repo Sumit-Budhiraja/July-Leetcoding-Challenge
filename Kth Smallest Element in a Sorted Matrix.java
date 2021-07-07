90feb357447a6e03c36c59ce8b01e545683089de
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2021-07-07 23:23:34 +0530

    Add files via upload

---
+++ Kth Smallest Element in a Sorted Matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthSmallest(int[][] M, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        for(int[] R : M){
+            for(int a : R){
+                heap.add(a);
+                if(heap.size() > k)
+                    heap.poll();
+            }
+        }
+        return heap.peek();
+    }
+}
